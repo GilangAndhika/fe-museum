9ef6e17a472e922953cd1adf33d0f09ccf7fe400
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 20:01:29 +0700

    Tes

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-import { isiTabel } from "./controller/get.js";
+import { isiTabel } from "../controller/get.js";
 import { urlAPI } from "./config/url.js";
 get(urlAPI, isiTableCollections);
 
