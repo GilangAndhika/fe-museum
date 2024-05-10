66ec632cd544392778f3af247e67c04c42f722b2
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 20:49:32 +0700

    tes

--- js/controller/get.js
+++ js/controller/get.js
@@ -7,63 +7,19 @@ export function isiTableCollections(results) {
 }
 
 function isiRow(value) {
-    if (!value || typeof value !== 'object') {
-      throw new Error('The input value must be a non-empty object.');
-    }
-  
-    const requiredProperties = [ 'name', 'location', 'establishedYear', 'description', 'website', 'openingHours'];
-    requiredProperties.forEach(prop => {
-      if (!value.hasOwnProperty(prop) || value[prop] === null || value[prop] === undefined) {
-        throw new Error(`Missing required property: ${prop}`);
-      }
-    });
-  
-    if (!value.hasOwnProperty('name') || !value.hasOwnProperty('location') || !value.hasOwnProperty('establishedYear')) {
-      throw new Error('The collections object must have name, location, and establishedYear properties.');
-    }
-  
-    if (typeof value.name !== 'string' || typeof value.location !== 'string' || typeof value.establishedYear !== 'number') {
-      throw new Error('The name, location, and establishedYear properties must be a string and a number, respectively.');
-    }
-  
-    if (typeof value.description !== 'string' || typeof value.website !== 'string' || typeof value.openingHours !== 'string') {
-      throw new Error('The description, website, and openingHours properties must be a string.');
-    }
-  
-    if (!Array.isArray(value.jam_kerja) || value.jam_kerja.length === 0) {
-      value.jam_kerja = [{ durasi: 0, jam_masuk: 'Belum diatur', jam_keluar: 'Belum diatur' }];
-    }
-  
+
     let content =
-      isiTabel.replace('#NAMA#', value.name)
-        .replace('#LOKASI#', value.location)
-        .replace('#EST#', value.establishedYear)
-        .replace('#DESKRIPSI#', value.description)
-        .replace('#WEBSITE#', value.website)
-        .replace('#HARIKERJA#', value.openingHours)
-        .replace('#JAMKERJA#', value.jam_kerja ? value.jam_kerja[0].durasi + " jam" : "Belum diatur")
-        .replace('#JAMMASUK#', value.jam_kerja ? value.jam_kerja[0].jam_masuk + " WIB" : "Belum diatur")
-        .replace('#JAMKELUAR#', value.jam_kerja ? value.jam_kerja[0].jam_keluar + " WIB" : "Belum diatur")
-        .replace('#WARNA#', getRandomColor())
-        .replace(/#WARNALOGO#/g, getRandomColorName());
-  
+
+        isiTabel.replace("#NAMA#", value.collections.name)
+            .replace("#LOKASI#", value.location)
+            .replace("#EST#", value.establishedYear)
+            .replace("#DESKRIPSI#", value.description)
+            .replace("#WEBSITE#", value.website)
+            .replace("#HARIKERJA#", value.openingHours)
+            .replace("#JAMKERJA#", value.jam_kerja ? value.jam_kerja[0].durasi + " jam" : "Belum diatur")
+            .replace("#JAMMASUK#", value.jam_kerja ? value.jam_kerja[0].jam_masuk + " WIB" : "Belum diatur")
+            .replace("#JAMKELUAR#", value.jam_kerja ? value.jam_kerja[0].jam_keluar + " WIB" : "Belum diatur")
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner("iniTabel", content);
-  }
-
-// function isiRow(value) {
-
-//     let content =
-
-//         isiTabel.replace("#NAMA#", value.collections.name)
-//             .replace("#LOKASI#", value.location)
-//             .replace("#EST#", value.establishedYear)
-//             .replace("#DESKRIPSI#", value.description)
-//             .replace("#WEBSITE#", value.website)
-//             .replace("#HARIKERJA#", value.openingHours)
-//             .replace("#JAMKERJA#", value.jam_kerja ? value.jam_kerja[0].durasi + " jam" : "Belum diatur")
-//             .replace("#JAMMASUK#", value.jam_kerja ? value.jam_kerja[0].jam_masuk + " WIB" : "Belum diatur")
-//             .replace("#JAMKELUAR#", value.jam_kerja ? value.jam_kerja[0].jam_keluar + " WIB" : "Belum diatur")
-//             .replace("#WARNA#", getRandomColor())
-//             .replace(/#WARNALOGO#/g, getRandomColorName());
-//     addInner("iniTabel", content);
-// }
+}
