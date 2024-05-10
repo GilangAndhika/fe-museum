b18f77b4136e8de360e4f624f7a009b1b73424c1
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 22:14:40 +0700

    tes

--- js/controller/get.js
+++ js/controller/get.js
@@ -4,13 +4,14 @@ import { isiTabel } from "../temp/table.js";
 
 // Mengubah struktur objek dari MongoDB agar sesuai dengan yang diharapkan
 function convertMongoDBData(results) {
-    return results.map(obj => ({
-        name: obj.Value.name,
-        location: obj.Value.location,
-        establishedYear: obj.Value.establishedYear,
-        description: obj.Value.description,
-        website: obj.Value.website,
-        openingHours: obj.Value.openingHours,
+    return results.map(result => ({
+        name: result.name,
+        location: result.location,
+        establishedYear: result.establishedYear,
+        description: result.description,
+        website: result.website,
+        openingHours: result.openingHours,
+        jam_kerja: result.jam_kerja // Properti jam_kerja diambil langsung dari MongoDB
     }));
 }
 
@@ -29,6 +30,9 @@ function isiRow(result) {
             .replace("#DESKRIPSI#", result.description)
             .replace("#WEBSITE#", result.website)
             .replace("#HARIKERJA#", result.openingHours)
+            .replace("#JAMKERJA#", result.jam_kerja ? result.jam_kerja[0].durasi + " jam" : "Belum diatur")
+            .replace("#JAMMASUK#", result.jam_kerja ? result.jam_kerja[0].jam_masuk + " WIB" : "Belum diatur")
+            .replace("#JAMKELUAR#", result.jam_kerja ? result.jam_kerja[0].jam_keluar + " WIB" : "Belum diatur")
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner("iniTabel", content);
