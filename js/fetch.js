193c0e384d0e6391f31928e7ec965de6fa238a70
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 19:47:10 +0700

    Fix Bug

--- js/fetch.js
+++ js/fetch.js
@@ -1,21 +1,6 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-import { setInner } from "https://bukulapak.github.io/element/process.js";
-let urlAPI = "https://ws-gilang-ats-65a7295ec924.herokuapp.com/collections";
-get(urlAPI,isiTableCollections);
-function isiTableCollections(results){
-    console.log(results);
-    results.forEach(isiRow);
-}
-function isiRow(value) {
-    let content = 
-    iniTabel.replace("#NAMA#", value.nama)
-            .replace("#NOHP#", value.location)
-            .replace("#JABATAN#", value.establishedYear)
-            .replace("#LOKASI#", value.description)
-            .replace("#STATUS#", value.checkin)
-            .replace("#HARIKERJA#", value.biodata.hari_kerja)
-            .replace("#WARNA#", getRandomColor())
-            .replace(/#WARNALOGO#/g, getRandomColorName());
-        addInner("iniTabel", content);
-}
+import { isiTabel } from "./controller/get.js";
+import { urlAPI } from "./config/url.js";
+get(urlAPI, isiTableCollections);
+
 
