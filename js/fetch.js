ed4456699d0a39fed7d3a084a7611ca88f11c3ad
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 14:32:10 +0700

    Halah capek

--- js/fetch.js
+++ js/fetch.js
@@ -1,25 +1,21 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
 import { setInner } from "https://bukulapak.github.io/element/process.js";
-
 let urlAPI = "https://ws-gilang-ats-65a7295ec924.herokuapp.com/collections";
-
-get(urlAPI, isiTableCollections);
-
+get(urlAPI,isiTableCollections);
 function isiTableCollections(results){
     console.log(results);
     results.forEach(isiRow);
 }
-
 function isiRow(value) {
     let content = 
-    isiTabel.replace("#NAME#", value.name)
-            .replace("#DESCRIPTION#", value.description)
-            .replace("#YEAR#", value.year)
-            .replace("#ARTIST#", value.artist)
-            .replace("#MEDIUM#", value.medium)
-            .replace("#DIMENSIONS#", value.dimensions)
-            .replace("#ORIGIN#", value.origin)
-            .replace("#ACQUISITION#", value.acquisition)
-            .replace("#CONDITION#", value.condition);
-    addInner("iniTabel", content);
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
+
--- js/temp/table.js
+++ js/temp/table.js
@@ -7,21 +7,24 @@ export let isiTabel =
                 <input class="w-4 h-4 bg-white rounded" type="checkbox">
             </div>
             <div class="w-auto p-2">
-                <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">#NAME#</div>
+                <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
             </div>
             <div class="w-auto p-2">
-                <p class="text-xs font-semibold text-coolGray-800">#DESCRIPTION#</p>
-                <p class="text-xs font-medium text-coolGray-500">#YEAR#</p>
+                <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
+                <p class="text-xs font-medium text-coolGray-500">#NOHP#</p>
             </div>
         </div>
     </th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#ARTIST#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#MEDIUM#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#DIMENSIONS#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#ORIGIN#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#ACQUISITION#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#JABATAN#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#STATUS#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#LOKASI#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#HARIKERJA#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#JAMKERJA#</th>
     <th class="whitespace-nowrap px-4 bg-white text-left">
-        <p class="text-xs font-medium text-coolGray-500">#CONDITION#</p>
+        <div class="w-auto p-2">
+        <p class="text-xs font-semibold text-coolGray-800">#JAMMASUK#</p>
+        <p class="text-xs font-medium text-coolGray-500">#JAMKELUAR#</p>
+        </div>
     </th>
     <th class="whitespace-nowrap pr-4 bg-white text-sm font-medium text-coolGray-800">
         <svg class="ml-auto" width="16" height="16" viewbox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -29,4 +32,4 @@ export let isiTabel =
         </svg>
     </th>
 </tr>
-`;
+`
