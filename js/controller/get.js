d4010b8238394848fb62a081b66c7b2ea1b35ea0
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 21:43:03 +0700

    resuuuuu

--- js/controller/get.js
+++ js/controller/get.js
@@ -21,19 +21,19 @@ export function isiTableCollections(results) {
     convertedResults.forEach(isiRow);
 }
 
-function isiRow(result) {
-    console.log('Result:', result);
-    let content =
-        isiTabel.replace("#NAMA#", result.name)
-            .replace("#LOKASI#", result.location)
-            .replace("#EST#", result.establishedYear)
-            .replace("#DESKRIPSI#", result.description)
-            .replace("#WEBSITE#", result.website)
-            .replace("#JAMOPERASI#", result.openingHours)
-            .replace("#JAMKERJA#", result.jam_kerja ? result.jam_kerja[0].durasi + " jam" : "Belum diatur")
-            .replace("#JAMMASUK#", result.jam_kerja ? result.jam_kerja[0].jam_masuk + " WIB" : "Belum diatur")
-            .replace("#JAMKELUAR#", result.jam_kerja ? result.jam_kerja[0].jam_keluar + " WIB" : "Belum diatur")
-            .replace("#WARNA#", getRandomColor())
-            .replace(/#WARNALOGO#/g, getRandomColorName());
-    addInner("iniTabel", content);
-}
+// function isiRow(result) {
+//     console.log('Result:', result);
+//     let content =
+//         isiTabel.replace("#NAMA#", result.name)
+//             .replace("#LOKASI#", result.location)
+//             .replace("#EST#", result.establishedYear)
+//             .replace("#DESKRIPSI#", result.description)
+//             .replace("#WEBSITE#", result.website)
+//             .replace("#JAMOPERASI#", result.openingHours)
+//             .replace("#JAMKERJA#", result.jam_kerja ? result.jam_kerja[0].durasi + " jam" : "Belum diatur")
+//             .replace("#JAMMASUK#", result.jam_kerja ? result.jam_kerja[0].jam_masuk + " WIB" : "Belum diatur")
+//             .replace("#JAMKELUAR#", result.jam_kerja ? result.jam_kerja[0].jam_keluar + " WIB" : "Belum diatur")
+//             .replace("#WARNA#", getRandomColor())
+//             .replace(/#WARNALOGO#/g, getRandomColorName());
+//     addInner("iniTabel", content);
+// }
