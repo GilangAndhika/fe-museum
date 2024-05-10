62235a6f38b75f729a6e00f03bf55a7fd23ed8ab
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-10 21:10:15 +0700

    Tes lagi

--- js/controller/get.js
+++ js/controller/get.js
@@ -2,9 +2,23 @@ import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "../temp/table.js";
 
+// Mengubah struktur objek dari MongoDB agar sesuai dengan yang diharapkan
+function convertMongoDBData(results) {
+    return results.map(result => ({
+        name: result.name,
+        location: result.location,
+        establishedYear: result.establishedYear,
+        description: result.description,
+        website: result.website,
+        openingHours: result.openingHours,
+        jam_kerja: result.jam_kerja // Properti jam_kerja diambil langsung dari MongoDB
+    }));
+}
+
 export function isiTableCollections(results) {
     console.log('Results:', results);
-    results.forEach(isiRow);
+    const convertedResults = convertMongoDBData(results);
+    convertedResults.forEach(isiRow);
 }
 
 function isiRow(value) {
